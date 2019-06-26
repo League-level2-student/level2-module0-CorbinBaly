475aa3eb73d34e8400c700922b8b800ce10cfbd5
league <league@wts9.attlocal.net>
2019-06-26 16:28:22 -0700

    finished arraycheatsheet, worked on robotrace

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,45 @@
 package arrays;
 
+import java.lang.reflect.Array;
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
+
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		
-		//2. print the third element in the array
-		
-		//3. set the third element to a different value
-		
-		//4. print the third element again
-		
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
-		//6. make an array of 50 integers
-		
-		//7. use a for loop to make every value of the integer array a random number
-		
-		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		int smoll= 0;
+		// 1. make an array of 5 Strings
+		String[] sentences = new String[5];
+		// 2. print the third element in the array
+		System.out.println(sentences[3]);
+		// 3. set the third element to a different value
+		sentences[3] = "Different Value";
+		// 4. print the third element again
+		System.out.println(sentences[3]);
+		// 5. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < sentences.length; i++) {
+			System.out.println(sentences[i]);
+		}
+		// 6. make an array of 50 integers
+		int numbers[] = new int[50];
+		// 7. use a for loop to make every value of the integer array a random number
+		for (int i = 0; i < numbers.length; i++) {
+			Random rand = new Random();
+			int value = rand.nextInt(999);
+			numbers[i] = value;
+			System.out.println(numbers[i]);
+		}
+
+		// 8. without printing the entire array, print only the smallest number in the
+		// array
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] < i++) {
+				smoll = numbers[i];
+			}
+		}
+		System.out.println(smoll);
+		// 9 print the entire array to see if step 8 was correct
+
+		// 10. print the largest number in the array.
 	}
 }
